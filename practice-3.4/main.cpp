bef09bb52fb9b0dab8747a678e133dc241d57087
Jeongyong Lee <hua9378@gmail.com>
2023-05-03 13:52:22 +0900

    feat: Harris corner detector

---
+++ practice-3.4/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+const string imgPath = "../data/Practice3/butterfly.jpg";
+
+void cornerHarris(int, void*) {
+}
+
+int main(int argc, char** argv) {
+}
